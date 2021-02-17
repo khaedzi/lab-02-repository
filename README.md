2a91e3e5559a4662233160b87521ef576ccd748a
khaedzi <khaled.ziadeh@yahoo.com>
2021-02-17 07:53:14 +0200

    sortEdit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# lab-02-repository
+# lab-02
 
+Number  of feature:Pagination,Templating ,Styling with Flexbox ,Sort the images 4
+
+Estimate of time needed to complete: 3 hour
+
+Start time:12:30 
+
+Finish time: 7:25
+Actual time needed to complete: 6 hour
--- js/app.js
+++ js/app.js
@@ -1,82 +1,150 @@
 'use strict';
-console.log("d");
-var arr=[];
-function Dog(name, img, desc,key) {
-  this.name = name;
-  this.img = img;
-  this.descreption = desc;
-  this.key=key;
-  arr.push(key);
-  
+let dogs = [];
+
+function Dog(dog) {
+  for (let key in dog) {
+    this[key] = dog[key];
+  }
+}
+Dog.prototype.setPageUrl = function (url) {
+  this.url = url;
+}
+Dog.prototype.render = function () {
+  let template = $('#template').html();
+  return Mustache.render(template, this);
 }
 
-
-
-Dog.prototype.render = function () {
-  let section = $('.template').clone();
-  $('main').append(section);
-  section.find('h2').text(this.name);
-  section.find('img').attr('src', this.img);
-  section.find('p').text(this.descreption);
-  section.removeClass('template');
-  
-  section.addClass(this.key);
-
-};
-
-function populateDogsData() {
+function populateDogsData(url) {
+  dogs = [];//remove elements
+  if (url == 0) {
+    url = 'data/page-1.json';
+  }
   const ajaxSettings = {
     method: 'get',
     dataType: 'json'
   };
 
-  $.ajax('data/page-1.json', ajaxSettings)
+  $.ajax(url, ajaxSettings)
     .then(data => {
       data.forEach(element => {
-        let jsDog = new Dog(element.title, element.image_url,element.description,element.keyword);
-        jsDog.render();
+        let dog = new Dog(element);
+        dog.setPageUrl(url);
+        dogs.push(dog);
       });
-
-      renderKeywordsOption(arr);
+      renderPage(dogs);
+      renderKeywordsOption(url);
     });
 }
+function renderPage(dogs) {
+ 
+  $(".main-container").html('');  //remove sibilings
+  dogs.forEach(element => {
+    var section = $('<section></section>');
+    section.addClass(element.keyword);
+    section.addClass("margin-lt-8");
+    section.addClass("padding-ltr-8");
+    section.append(element.render());
+    $('.main-container').append(section);
 
-function renderKeywordsOption(array) {
-    //console.log(array);
-    var arraySet=getSet(array);
-    //console.log(arraySet);
-    $('document').ready(() =>{
-        
-      arraySet.forEach(element => {
-        var option=$('<option></option>');
-        option.text(element);
-        option.on('click',clickHandler)
-        $('#keywords').append(option); 
-      });
-      function clickHandler() {
-        console.log(this.value);
-        $('section').find('option')
-        var ignordeSections=document.getElementsByTagName('section');
-        for (let index = 0; index < ignordeSections.length; index++) {
-            ignordeSections[index].classList.add('template');  
-        }
-        var selectedSection=document.getElementsByClassName(this.value);
-        for (let index = 0; index < selectedSection.length; index++) {
-            selectedSection[index].classList.remove('template');  
-        }
-        console.log(sections);
-         
+  });
+
+}
+function renderKeywordsOption(url) {
+  var keywords = [];
+  for (let index = 0; index < dogs.length; index++) {
+    if (dogs[index].url == url) {
+      keywords.push(dogs[index].keyword);
+    }//end if  
+  }
+  keywords = getSet(keywords);
+  $('select').html('');
+  var option = $('<option></option>');
+  option.text('select filter');
+  option.val("select filter");
+  $('select').append(option);
+  $('document').ready(() => {
+    keywords.forEach(element => {
+      option = $('<option></option>');
+      option.text(element);
+      option.on('click', clickHandler)
+      $('select').append(option);
+    });
+    function clickHandler() {
+      if(this.value=="select filter") return;
+      //bug
+      $('section').addClass('template');
+      $('.' + this.value).removeClass('template');
     }
-    $('#keywords').on('change',clickHandler);
-      
-    });
-    
-
+    $('select').on('change', clickHandler);
+  });
 }
-
-
 function getSet(array) {
-    var arraySet=new Set(array);
-    return Array.from(arraySet);
+  var arraySet = new Set(array);
+  return Array.from(arraySet);
 }
-$('document').ready(populateDogsData);
+
+$('document').ready(() => {
+
+  $('#page1_btn').on('click', () => {
+    populateDogsData('data/page-1.json');
+  });
+  $('#page2_btn').on('click', () => {
+    populateDogsData('data/page-2.json');
+  });
+
+  $('#sortTitle_btn').on('click', () => {
+    sort('title');
+  });
+  $('#sortHorn_btn').on('click', () => {
+    sort('horns');
+  });
+
+});
+
+function sort(sortBy) {
+  function compare(a, b) {
+    if (a[sortBy] < b[sortBy]) {
+      return -1;
+    } else if (a[sortBy] > b[sortBy]) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  dogs.sort(compare);
+  //render
+  //$('#select option:nth-child(1)').val();
+  var filter = document.getElementById("filter").value;
+  if(filter=="select filter") {
+    renderPage(dogs);
+    return;
+  }
+  renderFilterdPage(dogs, filter);//depend on selected filter
+}
+function renderFilterdPage(dogs,filter) {
+  
+  console.log(filter);
+  console.log(dogs.length)
+  $(".main-container").html('');  //remove sibilings
+
+  dogs.forEach(element => {
+    var section = $('<section></section>');
+    section.addClass(element.keyword);
+    section.addClass("margin-lt-8");
+    section.addClass("padding-ltr-8");
+    if (element.keyword!=filter) {
+      section.addClass("template");
+    }else{
+      console.log('else')
+      section.removeClass("template");
+    }
+
+    section.append(element.render());
+    $('.main-container').append(section);
+    
+    
+  });
+}
+/*exexutable*/
+$('document').ready(populateDogsData(0));
+
